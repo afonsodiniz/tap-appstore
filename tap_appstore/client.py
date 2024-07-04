5c6819c1df099db2aedb3cf694ed07ab7d2e78d4
Afonso Diniz <afonso@rauva.com>
2024-07-04 15:22:37 +0100

    chore: change timedelta of UTC, because Airflow localtime

--- tap_appstore/client.py
+++ tap_appstore/client.py
@@ -41,7 +41,7 @@ class AppStoreStream(RESTStream):
         private_key = base64.b64decode(encoded_key).decode('utf-8')
     
 
-        current_local_time = datetime.utcnow() + timedelta(hours=1) 
+        current_local_time = datetime.utcnow() # + timedelta(hours=1) # uncomment if locally testing
         expiration_time = current_local_time + timedelta(minutes=10)
 
         iat = int(current_local_time.timestamp())
